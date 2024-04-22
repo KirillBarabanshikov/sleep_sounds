7806d363d3abcb6bcd98aa1e189e2b1349379441
Kirill <kirillbar24@gmail.com>
2024-04-22 09:39:30 +0400

    fix player_bottom_sheet

--- lib/features/player/player_bottom_sheet/ui/player_bottom_sheet.dart
+++ lib/features/player/player_bottom_sheet/ui/player_bottom_sheet.dart
@@ -19,53 +19,94 @@ class PlayerBottomSheet extends StatelessWidget {
     final theme = Theme.of(context);
 
     return AppBottomSheet(
-      child: Column(
-        children: [
-          const SizedBox(height: 43),
-          SvgPicture.asset(
-            'assets/icons/popover.svg',
-            colorFilter: ColorFilter.mode(
-              theme.colorScheme.tertiary,
-              BlendMode.srcIn,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 43, bottom: 98),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/popover.svg',
+              colorFilter: ColorFilter.mode(
+                theme.colorScheme.tertiary,
+                BlendMode.srcIn,
+              ),
             ),
-          ),
-          PackCover(imageUrl: pack.imageCover, borderRadius: 24),
-          const SizedBox(height: 24),
-          Text(pack.name, style: theme.textTheme.bodyLarge),
-          const SizedBox(height: 2),
-          Text(song.name, style: theme.textTheme.titleLarge),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
+            Column(
               children: [
-                Container(
-                  padding: EdgeInsets.zero,
-                  margin: EdgeInsets.zero,
-                  child: Slider(
-                    value: 0.5,
-                    min: 0,
-                    max: 1,
-                    onChanged: (value) {},
-                    secondaryTrackValue: 0.6,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '00:10',
-                      style: theme.textTheme.bodyMedium?.copyWith(color: Colors.white),
-                    ),
-                    Text(
-                      '07:10',
-                      style: theme.textTheme.bodyMedium?.copyWith(color: Colors.white),
-                    ),
-                  ],
-                )
+                PackCover(imageUrl: pack.imageCover, borderRadius: 24),
+                const SizedBox(height: 24),
+                Text(pack.name, style: theme.textTheme.bodyLarge),
+                const SizedBox(height: 2),
+                Text(song.name, style: theme.textTheme.titleLarge),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.zero,
+                    margin: EdgeInsets.zero,
+                    child: Slider(
+                      value: 0.5,
+                      min: 0,
+                      max: 1,
+                      onChanged: (value) {},
+                      secondaryTrackValue: 0.6,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '00:10',
+                        style: theme.textTheme.bodyMedium?.copyWith(color: Colors.white),
+                      ),
+                      Text(
+                        '07:10',
+                        style: theme.textTheme.bodyMedium?.copyWith(color: Colors.white),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.fast_rewind,
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                  padding: const EdgeInsets.all(18),
+                ),
+                const SizedBox(width: 32),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.play_arrow,
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                  padding: const EdgeInsets.all(18),
+                ),
+                const SizedBox(width: 32),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.fast_forward,
+                    size: 36,
+                    color: Colors.white,
+                  ),
+                  padding: const EdgeInsets.all(18),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
